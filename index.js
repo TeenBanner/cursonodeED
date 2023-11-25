f20f79bd4db5c5f075c48dfdb2c4ff9d2b79f840
alfonso <alfonso.dancerod@gmail.com>
2023-11-24 23:36:31 -0600

    se agrego un idex.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const server = http.createServer(onRequest)
+const fs = require('fs')
 const port = 3000
+
 
 server.listen(port,() =>{
     console.log(`mi servidor esta corriendo en localhost:${port}`)
@@ -8,7 +10,8 @@ server.listen(port,() =>{
 
 function onRequest(req, res){
     console.log('se ha detectado una peticion')
-    res.setHeader('content-type', 'text/plain')
-    res.write('hola mundo')
+    res.setHeader('content-type', 'text/html')
+    res.write('<h1>Bienvenidos all curso de Nodejs</h1>')
+    res.write('<h2>Pagina de prueba</h2>')
     res.end()
 }
