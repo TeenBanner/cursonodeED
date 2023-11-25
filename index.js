290b3f47576b16ae44ae440bd7a970f66207001b
alfonso <alfonso.dancerod@gmail.com>
2023-11-25 00:29:15 -0600

    metods http

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const server = http.createServer(onRequest)
 const fs = require('fs')
 const port = 3000
+const qs = require('querystring')
 
 
 server.listen(port,() =>{
@@ -10,8 +11,60 @@ server.listen(port,() =>{
 
 function onRequest(req, res){
     console.log('se ha detectado una peticion')
-    res.setHeader('content-type', 'text/html')
-    res.write('<h1>Bienvenidos all curso de Nodejs</h1>')
-    res.write('<h2>Pagina de prueba</h2>')
-    res.end()
+    if(req.url === '/'){
+        fs.readFile('./index.html', (err, content) =>{
+            if(err){
+                if (err.code == 'ENOENT'){
+                    res.serStatus = 404
+                    console.log('no se a encontrado el archivo')
+                }else{
+                    res.setStatus = 500
+                    console.log('a ocurriod un error en el servidor')
+                }
+                console.log(`a ocurrido un error.  error: ${err}`)
+            }else{
+                res.serStatus = 202
+                res.setHeader('content-type', 'text/html')
+                res.write(content)
+                res.end()
+            }
+        })
+    }else if(req.url == '/users'){
+        if (req.method == 'GET'){
+            res.serStatus = 202
+            res.setHeader('content-type', 'text/html')
+            res.write('accediendo a usuarios')
+            res.end()
+        }else if (req.method == 'POST'){
+            let datos = ''
+            req.on('data', (d)=>{
+                datos += d
+            })
+            req.on('end',()=>{
+                let post = qs.parse(datos)
+                res.end(`datos recibidos: ` + post.nombre)
+            })
+            res.setHeader('content-type', 'text/html')
+            res.write('insertando usuarios')
+            res.end()
+        }else if (req.method == 'PUT'){
+            let datos = ''
+            req.on('data', (d)=>{
+                datos += d
+            })
+            req.on('end',()=>{
+                let post = qs.parse(datos)
+                res.end(`datos recibidos: ` + post.nombre)
+            })
+        }else if (req.method == 'DELETE'){
+            let datos = ''
+            req.on('data', (d)=>{
+                datos += d
+            })
+            req.on('end',()=>{
+                let post = qs.parse(datos)
+                res.end(`datos recibidos: `+ post.nombre)
+            })
+        }
+    }
 }
